76fb907e69858dffe844101882b7b23da396d53c
Kirill Zaidulin <zaidulinkirill@gmail.com>
2020-02-19 18:03:02 +1000

    Stash again

--- src/Form.vue
+++ src/Form.vue
@@ -1,5 +1,23 @@
 <script>
 import clone from 'clone';
+import { VRow, VCol, VTextField } from 'vuetify/lib/components';
+
+const types = {
+  text: {
+    component: VTextField,
+  },
+  row: {
+    component: VRow,
+    childrenResolver: (child, h) => {
+      const props = ['align-self', 'cols', 'lg', 'md', 'offset', 'offset-lg', 'offset-md', 'offset-sm', 'offset-xl', 'order', 'order-lg', 'order-md', 'order-sm', 'order-xl', 'sm', 'xl'];
+      const keys = Object.keys(child).filter((key) => props.includes(key));
+
+      return h(VCol, {
+        props: Object.assign({ cols: 12 }, ...keys.map((key) => ({ [key]: child[key] }))),
+      });
+    },
+  },
+};
 
 export default {
   name: 'VuetifySchemaForm',
@@ -8,12 +26,8 @@ export default {
       type: Object,
       default: () => ({}),
     },
-    gapped: {
-      type: Boolean,
-      default: false,
-    },
     fields: {
-      type: [Array, Object],
+      type: Object,
       default: null,
     },
     context: {
@@ -49,37 +63,42 @@ export default {
       }
     },
   },
-  render(createElement) {
-    const actualComponentTree = this.fields
-      ? fieldCompiler(this.fields.length ? { type: 'row', fields: this.fields } : this.fields)
-      : componentsTree;
+  render(h) {
 
-    return createElement(Form || VForm, {
-      props: {
-        lazyValidation: true,
-      },
-      class: 'py-2',
-      ref: 'editForm',
-      on: {
-        submit: (event) => {
-          this.submit();
-          event.preventDefault();
-        },
-      },
-    }, [
-      renderComponents(createElement, actualComponentTree, this.clone, {
-        globalProps: {
-          dense: this.$attrs.dense === undefined ? true : this.$attrs.dense,
-        },
-        globalClasses: {
-          ...this.gapped && { 'mb-1': true },
-        },
-        ...this.reactive && {
-          onInput: () => this.$emit('input', this.clone),
-          onChange: () => this.$emit('change', this.clone),
-        },
-      }),
-    ]);
   },
 };
+
+/*
+ library API v1.0
+  old =[
+    { label: 'Название', value: 'name', required: true, md: 6, lg: 4 },
+    { label: 'Slug', value: 'slug', type: 'slug', basedOn: 'name', required: true, md: 6, lg: 4 },
+    { label: 'Сортировка', value: 'sort', required: true, type: 'int', md: 6, lg: 4 },
+    { label: 'Стоимость', value: 'price', required: true, type: 'int', md: 6, lg: 4, suffix: '₽' },
+    { label: 'Акционная стоимость', value: 'discountPrice', type: 'int', md: 6, lg: 4, suffix: '₽' },
+    { label: 'Участвует в акции', value: 'withDiscount', type: 'bool', md: 6, lg: 4 },
+    { label: 'Категории',
+      value: 'categories',
+      type: 'remote',
+      multiple: true,
+      query: 'Category (where: {isRemoved: {_neq: true}}) { id name }',
+      itemText: 'name',
+      itemValue: 'id',
+      required: true,
+      outlined: true
+    },
+    { label: 'Изображения', value: 'images', type: 'files' },
+    { label: 'Описание (для SEO)', value: 'description', type: 'textArea', outlined: true, required: true },
+    { label: 'Описание', value: 'content', type: 'html', outlined: true, required: true }
+  ]
+
+  Цели: однозначно преобразовать JSON-описание в node-tree
+  {
+    type: 'row',
+    props: {},
+    children: [
+      { props: { label: 'Название', value: 'name', required: true, md: 6, lg: 4 } },
+    ]
+  }
+ */
 </script>
