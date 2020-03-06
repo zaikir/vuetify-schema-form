8183782c446584bea4f85fe408e60cf8b6793534
Kirill Zaidulin <zaidulinkirill@gmail.com>
2020-03-06 22:50:27 +0300

    fix

--- src/types.js
+++ src/types.js
@@ -18,9 +18,11 @@ export default {
       const keys = Object.keys(childProps).filter((key) => allProps.includes(key));
       const props = Object.assign({ cols: 12 }, ...keys.map((key) => ({ [key]: childProps[key] })));
 
-      return [{
-        component: VCol, type: 'col', props, children: [child],
-      }];
+      return !child
+        ? []
+        : [{
+          component: VCol, type: 'col', props, children: [child],
+        }];
     },
     postProcessProps: ({ props, item }) => ({ props: { ...props, item } }),
   },
