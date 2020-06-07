754cb60fa424718126971da5ca0b4642a3dcbced
Kirill Zaidulin <zaidulinkirill@gmail.com>
2020-06-07 20:00:12 +0300

    Fix

--- src/components/ComboboxField.vue
+++ src/components/ComboboxField.vue
@@ -31,7 +31,10 @@ export default {
 };
 </script>
 <style>
-.vdk-combobox-field.v-input--dense .v-select__slot {
-  margin-top: -2px !important;
+.vdk-autocomplete-field.v-input--dense .v-select__slot {
+  margin-top: -1px !important;
+}
+.vdk-autocomplete-field .v-input__slot {
+  padding-bottom: 1px;
 }
 </style>
